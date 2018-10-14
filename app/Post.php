dd604ea3121987dca4ae002ea5187f44d7bf5c4b
Vladimir Ivanyuk <ivanyuk85vladimir@gmail.com>
2018-10-14 11:58:58 +0300

    Initial commit

---
+++ app/Post.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
+
+class Post extends Model
+{
+    use Sluggable;
+
+    protected $fillable = ['title', 'content'];
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class,
+            'posts_tags',
+            'post_id',
+            'tag_id');
+    }
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+    public static function add($fields)
+    {
+        $post = new static;
+        $post->fill($fields);
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+    public function remove()
+    {
+        Storage::delete('uploads/'. $this->image);
+        $this->delete();
+    }
+    public function uploadImage($image)
+    {
+        if($image==null){ return;}
+        Storage::delete('uploads/'. $this->image);
+        $filename = str_random(10) . '.' .$image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+    public function setCategory($id)
+    {
+        
+    }
+}
